02ca213c91d8c52acb737b4037f1b7364e60dd6f
NinaAtanaskovic <nina.atanaskovic@gmail.com>
2026-04-29 19:14:55 +0200

    frontend za svaki product + backend pocetak

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Outlet } from "react-router-dom"
 import { Container } from 'react-bootstrap'
 import HomeScreen from "./screens/HomeScreen"
-const App =()=> {
+const App =() => {
   return (
     <>
-    <main className="py-3">
     <Header />
+    <main className="py-3">
+    
     <Container>
-    <HomeScreen />
+    <Outlet />
     </Container>
     </main>
     <Footer />
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button, Badge, CardBody } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../product_list";
+
+const ProductScreen = ()=> {
+    const { id: productId } = useParams ()
+    const product = products.find((p) => p._id === productId);
+    console.log(product);
+    return <>
+    <Link className="btn btn-outline-secondary mb-4 " to='/'>Nazad</Link>
+    <Card className="border-0 shadow-sm p-4 mb-4 ">
+        <Row className="align-items-center"></Row>
+        <Col md={8}>
+        <h2 className="mb-2">{product.name}</h2> 
+        <Rating value={product.rating} text={`${product.numReviews} recenzija `}></Rating>
+
+        </Col>
+        <Col md={4} className="text-md-end mt-3 mt-md-0 ">
+        <h3 className="text-primary mb-0 ">{product.price.toFixed(2)} RSD</h3> 
+
+        </Col>
+    </Card>
+    <Row className="gy-4"> 
+        
+        <Col lg={8}>
+        <Card className="border-0 shadow-sm p-4">
+            <div className="text-center">
+                <Image
+                    src={product.image}
+                    alt={product.name}
+                    fluid 
+                    style={{ maxHeight: '500px', objectFit: 'contain'}}
+                />
+
+            </div>
+
+        </Card>
+        </Col>
+
+        <Col lg={4}>
+        <Card className="border-0 shadow-sm">
+            <CardBody>
+                <h4 className="mb-4 ">Informacije o proizvodu </h4>
+                <div className="d-flex justify-content-between mb-3">
+                    <span> 
+                        Kategorija:
+                    </span>
+                    {product.category}
+
+                </div>
+                <div className="d-flex justify-content-between slign-items-center mb-4">
+                    <span>Status:</span>
+                    {product.countInStock > 0 ? (
+                        <Badge bg='success'>Dostupno</Badge>
+                    ): (
+                        <Badge bg='danger'>Nije dostupno</Badge>
+                    )}
+                </div>
+                <div className="d-grid">
+                    <Button className="add-to-cart-btn" type="button" disabled={product.countInStock===0}
+                    >Dodaj u korpu
+                    </Button>
+
+                </div>
+            </CardBody> 
+
+        </Card>
+        
+        </Col>
+    </Row>
+    <Card className="border-0 shadow-sm mt-4">
+        <CardBody>
+            <h4 className="mb-3">Opis proizvoda</h4>
+            <p className="text-muted mb-0">{product.description}</p>
+        </CardBody>
+    </Card>
+    </>
+}
+export default ProductScreen
